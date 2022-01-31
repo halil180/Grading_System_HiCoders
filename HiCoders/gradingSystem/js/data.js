0138300ffb3790294d4b1675408ef84a78369a99
halil180 <halilkom180@.com>
2022-01-31 01:45:30 +0100

    hicoders grading system

--- HiCoders/gradingSystem/js/data.js
+++ HiCoders/gradingSystem/js/data.js
@@ -75,7 +75,7 @@ let classA = [{
     },
     {
         studentName: "Julian",
-        profilePicture: '../gradingSystem/imgs/classA/julian.png',
+        profilePicture: '../gradingSystem/imgs/classA/Julian.png',
         grades: [{
             math: 72,
             chemistry: 77,
